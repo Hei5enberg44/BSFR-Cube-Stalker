ddf484bef3c2f2f274e46db862ccd396163c985a
Hei5enberg <46035253+Hei5enberg44@users.noreply.github.com>
2021-10-08 13:27:04 +0200

    Contrôle si le fichier de configuration "config.json" est bien présent

--- bot.js
+++ bot.js
@@ -1,37 +1,49 @@
-const { Client, Intents } = require('discord.js')
-const crons = require('./controllers/crons')
-const Commands = require('./controllers/commands')
 const Logger = require('./utils/logger')
-const config = require('./config.json')
-
-Logger.log('[Application] Démarrage du bot')
-Logger.log('[Discord] Initialisation...')
 
 try {
-    const client = new Client({
-        intents: [
-            Intents.FLAGS.GUILDS,
-            Intents.FLAGS.GUILD_MEMBERS,
-            Intents.FLAGS.GUILD_MESSAGES,
-            Intents.FLAGS.GUILD_MESSAGE_REACTIONS
-        ]
-    })
+    const fs = require('fs')
 
-    client.once('ready', async () => {
-        Logger.log('[Discord] Initialisation terminée')
-    
-        // Chargement des commandes
-        const commands = new Commands(client)
-        await commands.load()
-        await commands.listen()
+    Logger.log('[Application] Démarrage du bot')
 
-        // Tâches planifiées
-        await crons.refreshLeaderboard(client)
-    
-        Logger.log('[Application] Le bot est prêt !')
-    })
-    
-    client.login(config.token)
+    // Chargement de la configuration du bot
+    if(!fs.existsSync('./config.json')) throw Error('Le fichier de configuration "config.json" est manquant')
+    const config = require('./config.json')
+
+    const { Client, Intents } = require('discord.js')
+    const Commands = require('./controllers/commands')
+    const crons = require('./controllers/crons')
+
+    Logger.log('[Discord] Initialisation...')
+
+    try {
+        const client = new Client({
+            intents: [
+                Intents.FLAGS.GUILDS,
+                Intents.FLAGS.GUILD_MEMBERS,
+                Intents.FLAGS.GUILD_MESSAGES,
+                Intents.FLAGS.GUILD_MESSAGE_REACTIONS
+            ]
+        })
+
+        client.once('ready', async () => {
+            Logger.log('[Discord] Initialisation terminée')
+        
+            // Chargement des commandes
+            const commands = new Commands(client)
+            await commands.load()
+            await commands.listen()
+
+            // Tâches planifiées
+            await crons.refreshLeaderboard(client)
+        
+            Logger.log('[Application] Le bot est prêt !')
+        })
+        
+        client.login(config.token)
+    } catch(error) {
+        Logger.log(`[Discord] [ERROR] Une erreur est survenue : ${error.message}`)
+    }
 } catch(error) {
-    Logger.log(`[Discord] [ERROR] Une erreur est survenue : ${error.message}`)
+    Logger.log(`[Application] [ERROR] Démarrage du bot impossible : ${error.message}`)
 }
+
