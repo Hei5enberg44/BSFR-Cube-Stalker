a2b1999d1163372c76b636f5cdc084ef1ee7da9d
Hei5enberg44 <antoineboulet44@gmail.com>
2022-08-29 17:20:32 +0200

    Ajout d'une mention pour chaque commande + Ajout de la commande /playlist

--- commands/help.js
+++ commands/help.js
@@ -16,21 +16,27 @@ module.exports = {
      */
     async execute(interaction) {
         try {
-            const commandsList = `\
-${inlineCode('/link')}: Lie votre profil ScoreSaber/BeatLeader à votre compte Discord\n\
-${inlineCode('/unlink')}: Supprime la liaison de votre profil ScoreSaber/BeatLeader à votre compte Discord\n\
-${inlineCode('/me')}: Affiche vos informations de joueur ou celles d'un autre joueur\n\
-${inlineCode('/card')}: Génère votre carte de joueur ou celle d'un autre joueur\n\
-${inlineCode('/ld')}: Affiche le classement du serveur (pp ou acc)\n\
-${inlineCode('/locateworld')}: Affiche votre position ou celle d'un autre joueur dans le classement mondial\n\
-${inlineCode('/top1')}: S\'inscire ou se désinscrire du top 1 FR\n\
-${inlineCode('/world')}: Affiche le classement mondial\
-`
-            
+            const commandsList = [
+                { name: 'link', description: 'Lie votre profil ScoreSaber/BeatLeader à votre compte Discord' },
+                { name: 'unlink', description: 'Supprime la liaison de votre profil ScoreSaber/BeatLeader à votre compte Discord' },
+                { name: 'me', description: 'Affiche vos informations de joueur ou celles d\'un autre joueur' },
+                { name: 'card', description: 'Génère votre carte de joueur ou celle d\'un autre joueur' },
+                { name: 'ld', description: 'Affiche le classement du serveur (pp ou acc)' },
+                { name: 'locateworld', description: 'Affiche votre position ou celle d\'un autre joueur dans le classement mondial' },
+                { name: 'top1', description: 'S\'inscire ou se désinscrire du top 1 FR' },
+                { name: 'world', description: 'Affiche le classement mondial' },
+                { name: 'playlist', description: 'Génère une playlist en fonction de vos maps jouées et de vos critères choisis en options de commande' },
+            ]
+
+            const help = commandsList.map(cl => {
+                const command = interaction.guild.commands.cache.find(c => c.name === cl.name)
+                return `</${command.name}:${command.id}>: ${cl.description}`
+            }).join('\n')
+
             const embed = new Embed()
                 .setColor('#000000')
                 .setTitle('Aide')
-                .addFields({ name: 'Liste des commandes', value: commandsList })
+                .addFields({ name: 'Liste des commandes', value: help})
             
             await interaction.reply({ embeds: [ embed ], ephemeral: true })
         } catch(error) {
@@ -42,3 +48,6 @@ ${inlineCode('/world')}: Affiche le classement mondial\
         }
     }
 }
+
+///////////////////////////////////////
+// Mentions des commandes dans l'aide
